8b8cc5d3aa01afaea35e3e7343da96343d810d98
w <pcranway@tuta.io>
2022-08-08 01:23:01 +0300

    finish stack (probably not)

--- vm/src/memory/stack.rs
+++ vm/src/memory/stack.rs
@@ -5,6 +5,8 @@ pub enum StackEntry {
     Primitive(Value),
 }
 
+pub type StackAddress = u64;
+
 pub struct StackMemory {
     pub data: Vec<StackEntry>,
 }
@@ -12,6 +14,16 @@ pub struct StackMemory {
 impl StackMemory {
     pub fn new(data: Vec<StackEntry>) -> Self {
         Self { data }
+    }
+
+    pub fn put(&mut self, entry: StackEntry) -> StackAddress {
+        self.data.push(entry);
+
+        self.data.len() as StackAddress - 1
+    }
+
+    pub fn get(&self, address: StackAddress) -> Option<&StackEntry> {
+        self.data.get(address as usize)
     }
 }
 
