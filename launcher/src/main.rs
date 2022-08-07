316e124fd76bf33d690aca130327b8196aad2388
w <pcranway@tuta.io>
2022-08-08 00:36:00 +0300

    use clap on launcher for parsing arguments

--- launcher/src/main.rs
+++ launcher/src/main.rs
@@ -1,3 +1,23 @@
+use std::{io, fs};
+
+use ::vm::vm::VM;
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[clap(version, about)]
+struct Args {
+    #[clap(short, long, value_parser)]
+    file: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    // read file
+    let instructions = fs::read_to_string(args.file).expect("file doesn't exist");
+
+    // create VM
+    let vm = VM::default();
+
+    todo!("execute instructions");
 }
